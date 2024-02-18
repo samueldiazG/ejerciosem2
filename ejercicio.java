c3920415d220532d3477fa77d11a3241d1d6b149
samueldiazG <160377716+samueldiazG@users.noreply.github.com>
2024-02-18 14:16:00 -0500

    ejercicios del 1-3

---
+++ ejercicio.java
@@ -0,0 +1,6 @@
+package ejercicio1;
+public class ejercicio {
+    public static void main(String[] args) {
+        System.out.println("principios de la programacion");
+    }
+}
---
+++ ejercicio3.java
@@ -0,0 +1,20 @@
+package ejercicio1;
+import java.util.Scanner;
+public class ejercicio3 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        int b,a;
+        System.out.println("escriba la base del triangulo");
+        b= leer.nextInt();
+        System.out.println("escriba la altura del triangulo");
+        a= leer.nextInt();
+        int area = b*a/2;
+        System.out.println("el area del triangulo:" + area);
+        
+
+
+        
+
+    }
+}
